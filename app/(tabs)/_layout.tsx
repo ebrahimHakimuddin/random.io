492cd3c1e9667a0bb72d0be309d99b30d32ed393
ebrahimHakimuddin <ebrahimhakimuddin@gmail.com>
2024-10-15 11:47:38 +0300

    reset project again

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,37 @@
-import { Tabs } from "expo-router";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import Octicons from "@expo/vector-icons/build/Octicons";
-export default function TabLayout(){
-    return (
-        <Tabs>
-          <Tabs.Screen name="numberRandomizer" options={{ title: 'Number Randomizer', tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'people-sharp' : 'people-outline'} color={color} size={24} />
-          ), }} />
-          <Tabs.Screen name="teamRandomizer" options={{ title: 'Team Randomizer', tabBarIcon:({color}) => (<Octicons name="number" size={24} color={color} />) }}/>
-        </Tabs>
-      );
-} 
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
